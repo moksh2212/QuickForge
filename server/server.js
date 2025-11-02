657390628615460855aca680473553ccfcb73556
Moksh Thakran <75446958+moksh2212@users.noreply.github.com>
2025-11-02 11:50:26 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,9 +13,10 @@ await connectCloudinary();
 app.use(
   cors({
     origin: "*",
-    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
-);
+)
 
 app.use(express.json());
 app.use(clerkMiddleware());
